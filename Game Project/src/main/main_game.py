6363d2cb5ac297b24fd0753440fb6d0bdb11d505
y0u1k2i3 <0gl78qs1ciwork@gmail.com>
2023-04-18 15:55:31 +0900

    Prototype

--- Game Project/src/main/main_game.py
+++ Game Project/src/main/main_game.py
@@ -2,9 +2,12 @@ import pygame
 import random
 import sys
 
+
+################################## Setting of game ##################################
+
 # The width and height of Game
-SCREEN_WIDTH = 1400
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 900
 
 # Initialize Pygame and make window
 pygame.init()
@@ -14,6 +17,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 background_color = (0, 0, 0)
 screen.fill(background_color)
 
+# Object image
 player_frame = ['../Assets/player_Frame1.png', '../Assets/player_Frame2.png',
                 '../Assets/player_Frame3.png', '../Assets/player_Frame4.png']
 enemy1_frame = ['../Assets/enemy1_Frame1.png', '../Assets/enemy1_Frame2.png',
@@ -21,80 +25,56 @@ enemy1_frame = ['../Assets/enemy1_Frame1.png', '../Assets/enemy1_Frame2.png',
 enemy2_frame = ['../Assets/enemy2_Frame1.png', '../Assets/enemy2_Frame2.png',
                 '../Assets/enemy2_Frame3.png', '../Assets/enemy2_Frame4.png']
 boss_frame = ['../Assets/boss_Frame1.png', '../Assets/boss_Frame2.png',
-                '../Assets/boss_Frame3.png', '../Assets/boss_Frame4.png']
+              '../Assets/boss_Frame3.png', '../Assets/boss_Frame4.png']
 
 active_Frame = 0
 
-#########################################################################
+
+################################## Class definition of game object ##################################
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
         # Load image of player
-        self.image = pygame.image.load(player_frame[active_Frame])
+        original_image = pygame.image.load(player_frame[active_Frame]) # need to check
+        self.image = pygame.transform.scale(original_image, (original_image.get_width//2, original_image.get_height//2)) # need to check
 
         # Setting the position of player
         self.rect = self.image.get_rect()
         self.rect.centerx = SCREEN_WIDTH / 2
-        self.rect.bottom = SCREEN_HEIGHT - 50
+        self.rect.bottom = SCREEN_HEIGHT - 10
+
+        self.speed = 13
 
     # Moveing the player according to the key input
     def update(self):
         key = pygame.key.get_pressed()
 
         if key[pygame.K_LEFT]:
-            self.rect.x -= 5
+            self.rect.x -= self.speed
 
         if key[pygame.K_RIGHT]:
-            self.rect.x += 5
+            self.rect.x += self.speed
 
         if key[pygame.K_UP]:
-            self.rect.y -= 5
+            self.rect.y -= self.speed
 
         if key[pygame.K_DOWN]:
-            self.rect.y += 5
+            self.rect.y += self.speed
 
+        # Stay on screen
+        if self.rect.left < 0:
+            self.rect.left = 0
 
-class Enemy1(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
 
-        # Load image of enemy1
-        self.image = pygame.image.load(enemy1_frame[active_Frame])
+        if self.rect.top < 0:
+            self.rect.top = 0
 
-        # Setting the position of enemy1
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(SCREEN_WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -50)
-
-    # Moveing enemy1
-    def update(self):
-        self.speed = random.randrange(2, 8)
-        self.rect.y += self.speed
-
-
-class Enemy2(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-
-        # Load image of enemy2
-        self.image = pygame.image.load(enemy2_frame[active_Frame])
-
-        # Setting the position of enemy2
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(SCREEN_WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -50)
-
-    # Moveing enemy2
-    def update(self):
-        self.speed = random.randrange(5, 15)
-        self.rect.y += self.speed
-
-
-class Boss_Enemy(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -116,22 +96,76 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.y += self.speed
 
         if self.rect.bottom < 0:
-            self.kill()  # need to check
+            self.kill()
 
-#########################################################################
+
+class Enemy1(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        # Load image of enemy1
+        self.image = pygame.image.load(enemy1_frame[active_Frame])
+
+        # Setting the position of enemy1
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(SCREEN_WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -50)
+        self.speed = random.randrange(8, 12)
+
+    # Move enemy1
+    def update(self):
+        self.rect.y += self.speed
+
+        # If enemy1 go out from screen, reapper on it
+        if self.rect.top > SCREEN_HEIGHT:
+            self.rect.x = random.randrange(SCREEN_WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -50)
+            self.speed = random.randrange(8, 12)
+
+
+class Enemy2(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        # Load image of enemy2
+        self.image = pygame.image.load(enemy2_frame[active_Frame])
+
+        # Setting the position of enemy2
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(SCREEN_WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -50)
+        self.speed = random.randrange(5, 15)
+
+    # Move enemy2
+    def update(self):
+        self.rect.y += self.speed
+
+        # If enemy1 go out from screen, reapper on it
+        if self.rect.top > SCREEN_HEIGHT:
+            self.rect.x = random.randrange(SCREEN_WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -50)
+            self.speed = random.randrange(5, 15)
+
+
+class Boss_Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+
+################################## Basic parts of the game ##################################
 
 # Make Player
 player = Player()
 
 # Make Enemy1 Group
 enemies1 = pygame.sprite.Group()
-for i in range(10):
+for i in range(6):
     enemy1 = Enemy1()
     enemies1.add(enemy1)
 
 # Make Enemy2 Group
 enemies2 = pygame.sprite.Group()
-for i in range(10):
+for i in range(3):
     enemy2 = Enemy2()
     enemies2.add(enemy2)
 
@@ -140,7 +174,7 @@ bullets = pygame.sprite.Group()
 
 clock = pygame.time.Clock()
 
-# Game_loop starts
+# Game_loop
 running = True
 while running:
     # Process the events
@@ -154,17 +188,17 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 # while pygame.time.get_ticks():
-                    bullet = Bullet(player.rect.centerx, player.rect.top)
-                    bullets.add(bullet)
+                bullet = Bullet(player.rect.centerx - 15, player.rect.top)
+                bullets.add(bullet)
 
     # Updata player
     player.update()
 
-    # # Updata enemy1
-    # enemy1.update()
+    # Updata enemy1
+    enemies1.update()
 
-    # # Updata enemy2
-    # enemy2.update()
+    # Updata enemy2
+    enemies2.update()
 
     # Updata bullet
     bullets.update()
@@ -174,21 +208,25 @@ while running:
         bullet_hits1 = pygame.sprite.spritecollide(bullet, enemies1, True)
         for bullet_hit1 in bullet_hits1:
             bullet.kill()
+            new_enemy1 = Enemy1()
+            enemies1.add(new_enemy1)
 
         bullet_hits2 = pygame.sprite.spritecollide(bullet, enemies2, True)
         for bullet_hit2 in bullet_hits2:
             bullet.kill()
+            new_enemy2 = Enemy2()
+            enemies2.add(new_enemy2)
 
     # Gameover
     player_hits1 = pygame.sprite.spritecollide(player, enemies1, True)
     if player_hits1:
-        running = False
         print("Game Over!!")
+        running = False
 
     player_hits2 = pygame.sprite.spritecollide(player, enemies2, True)
     if player_hits2:
-        running = False
         print("Game Over!!")
+        running = False
 
     # Draw the player, enemies, and bullet
     screen.blit(player.image, player.rect)
