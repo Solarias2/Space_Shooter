7167456f6655b05d8e6df824a909480e605cc609
y0u1k2i3 <0gl78qs1ciwork@gmail.com>
2023-04-27 14:59:30 +0900

    V2 is the latest.

--- Game Project/src/main/main_game_v2.py
+++ Game Project/src/main/main_game_v2.py
@@ -119,7 +119,7 @@ class Player(pygame.sprite.Sprite):
         self.last_shot = pygame.time.get_ticks()
 
         # Setting speed
-        self.speed = 15
+        self.speed = 16
 
         # Shoot_flag
         self.shoot_flag = False
